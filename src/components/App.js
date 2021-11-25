deb4a9a0f0ca042e173ab702867d6f259d76c182
Kat <bolivarkat@yahoo.com>
2021-11-25 13:03:27 -0600

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
 
+import {UserProvider} from "../context/user";
+import {ThemeProvider, ThemeContext} from "../context/theme";
+import DarkModeToggle from "./DarkModeToggle";
+
 function App() {
-  const [theme, setTheme] = useState("dark");
-  const [user, setUser] = useState(null);
+  const { theme } = useContext(ThemeContext);
   return (
-    <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
+    <main className ={theme}>
+      <UserProvider>
+        <Header/>
+        <Profile/>
+      </UserProvider>
+      <ThemeProvider>
+        <DarkModeToggle/>
+      </ThemeProvider>
     </main>
+    
   );
 }
 
